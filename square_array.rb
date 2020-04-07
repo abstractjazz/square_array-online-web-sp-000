096d00f3e9ce9dae38a34fc0f5cf8e6e6f565419
Evan Burton <evan@heyevan.com>
2020-04-07 07:32:34 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -10,6 +10,11 @@ end
 
 
 #def square_array(array)
+  #defines the #square_array method and takes in an argument of an array
 #new_array = array.collect {|index| index ** 2}
+  #defines a new variable 'new_array' and stores a new array inside of it, consisting of the squared values of 'index' at each iteration. 
+  #(This could have been any operation besides square. The method would work the same)
 #return new_array
+#this explicitly calls the variable we created, though maybe it's not neccessary, since .collect returns a new array anyway. however, 
+#the new variable will allow further work with the new array to be a lot easier. 
 #end
